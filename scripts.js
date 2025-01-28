a4a78b5fe59af9f2e83752d61cdbf8926732e1be
g-allen23 <geovaneallen8@gmail.com>
2025-01-27 22:23:32 -0300

    Aprimorando o JavaScript na aplicação, e adicionando comentários para facilitar na manutençaõ da aplicação.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,36 @@
-const convertButton =  document.querySelector(".convert-button")
+const convertButton = document.querySelector(".convert-button") // Mapeando o botão para conversão
+const currencySelect = document.querySelector("#convert") // Mapeando o select para saber para qual moeda vai converter
 
 
 function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const dolarToDay = 5.91
-    const convertedValue = inputCurrencyValue / dolarToDay
-    
-    console.log(convertedValue)
+    const inputCurrencyValue = document.querySelector(".input-currency").value // Mapeando o valor do input digitado pelo usuário
+    const currencyToConvert = document.querySelector(".value-to-convert") // Valor a ser convertido
+    const currencyConvertedValue = document.querySelector(".converted-value") // Valor já convertido
+
+    const dolarToDay = 5.91 // Valor que será calculado ao input digitado
+    const euroToDay = 6.20
+
+    if (currencySelect.value == "dolar") {
+        // Se o select estiver selecionado o valor de dolar entre aqui
+        currencyConvertedValue.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToDay) // Formatando o valor digitado em Dólar(moeda).
+    }
+
+    if (currencySelect.value == "euro") {
+        // Se o select estiver selecionado o valor de euro entre aqui
+        currencyConvertedValue.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToDay) // Formatando o valor digitado em Euro(moeda)
+    }
+
+    currencyToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue) // Formatando o valor digitado em Real(moeda).
+
 }
 
 convertButton.addEventListener("click", convertValues)
